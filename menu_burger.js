4eae154c03a354411328825ef960594d06d91f68
Nicos-08 <thierrylemoine136@gmail.com>
2023-03-03 18:12:19 +0100

    Flou fonctionnel

--- menu_burger.js
+++ menu_burger.js
@@ -1,7 +1,13 @@
 const logoMenuBurger = document.querySelector(".header-menu_burger");
+const filtre = document.querySelector(".header-filtre")
 let first = 0;
 
 const updateMenu = () => {
+    if (!filtre.classList.contains("header-filtre--active")) {
+        filtre.classList.add("header-filtre--active");
+    } else {
+        filtre.classList.remove("header-filtre--active");
+    }
     logoMenuBurger.classList.toggle("open");
     first++;
     if (first === 2) {
